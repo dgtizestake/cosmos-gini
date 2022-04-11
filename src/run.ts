3b3875c9424e60e54b72a0bc916c1aa6a142b56b
Dgtize Stake <contact@dgtize.ventures>
2022-04-12 01:06:09 +0300

    Add command for avg. Gini coefficient

---
+++ src/run.ts
@@ -0,0 +1,80 @@
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import pLimit from 'p-limit';
+import axios from 'axios';
+
+interface ValidatorResponse {
+  result: {
+    validators: {
+      voting_power: number;
+    }[];
+  };
+}
+
+const giniByHeight = async (rpc: string, height: number): Promise<number> => {
+  const {
+    data: {
+      result: { validators },
+    },
+  } = await axios.get<ValidatorResponse>(`${rpc}validators?height=${height}&per_page=200`); // Hacky circumvention of pagination
+  const votingPowers = validators.map(validator => validator.voting_power);
+
+  const sorted = votingPowers.map(n => Number(n)).sort((a, b) => a - b);
+  const sum = sorted.reduce((a, b) => a + b, 0);
+
+  // Formula taken from http://kimberlyfessel.com/mathematics/applications/gini-use-cases/ for strictly positive values
+  const numerator = sorted.reduce((a, b, i) => a + b * (2 * i - sorted.length - 1), 0);
+  const denominator = sorted.length * sum;
+
+  const coefficient = numerator / denominator;
+  console.log(`Computed Gini coefficient of ${coefficient} for height ${height}`);
+  return coefficient;
+};
+
+yargs(hideBin(process.argv))
+  .strict()
+  .command(
+    'gini',
+    'Calculate the Gini coefficient for apps built with Cosmos SDK',
+    yargs =>
+      yargs
+        .options({
+          rpc: {
+            describe: 'Tendermint RPC URL',
+            default: 'http://localhost:26657',
+            type: 'string',
+          },
+          concurrency: {
+            describe: 'Maximum number of RPC requests concurrently pending',
+            default: 3,
+            type: 'number',
+          },
+          startHeight: {
+            describe: 'The starting block height to include in the average calculation',
+            type: 'number',
+            demandOption: true,
+          },
+          endHeight: {
+            describe: 'The ending block height to include in the average calculation',
+            type: 'number',
+            demandOption: true,
+          },
+        })
+        .check(argv => {
+          if (argv.startHeight >= argv.endHeight) throw 'Start height must be less than end height';
+          return true;
+        }),
+    async argv => {
+      const { rpc, concurrency, startHeight, endHeight } = argv;
+      const limit = pLimit(concurrency);
+
+      const pool = [...Array(endHeight - startHeight)].map((_, i) => limit(() => giniByHeight(rpc, startHeight + i)));
+      const coefficients = await Promise.all(pool);
+
+      const avg = coefficients.reduce((a, b) => a + b) / coefficients.length;
+      console.log(
+        `${avg.toFixed(4)} avg. Gini coefficient for ${coefficients.length} blocks (${startHeight}-${endHeight})`
+      );
+    }
+  )
+  .help().argv;
